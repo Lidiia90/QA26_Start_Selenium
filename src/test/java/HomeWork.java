a67eef025d1d4ecc12e3ab6cd9a5046e2ad5f9ce
Lidiia90 <liduska200412@gmail.com>
2024-06-22 17:57:47 +0300

    Homework 1

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -30,39 +30,19 @@ public class HomeWork {
         WebElement input1 = wd.findElement(By.cssSelector("input"));
 
         //by Class
-        WebElement element = wd.findElement(By.className("navbar-logged_nav__2Hx7M"));
-        WebElement element1 = wd.findElement(By.cssSelector(".navbar-logged_nav__2Hx7M"));
-
         List<WebElement> containerList = wd.findElements(By.className("container"));
         List<WebElement> containerList1 = wd.findElements(By.cssSelector(".container"));
 
         //by id
-        WebElement container = wd.findElement(By.id("container"));
         WebElement container1 = wd.findElement(By.cssSelector("#root.container"));
 
         //by attribute
         WebElement inputName = wd.findElement(By.cssSelector("[placeholder='Email']"));
         WebElement inputName1 = wd.findElement(By.cssSelector("[placeholder='Password']"));
-        WebElement a2 = wd.findElement(By.cssSelector("[href='#home']"));
-        WebElement a3 = wd.findElement(By.cssSelector("[href='#about']"));
-        WebElement a4 = wd.findElement(By.cssSelector("[href='#contacts']"));
-        WebElement a5 = wd.findElement(By.cssSelector("[href='#add']"));
-
-        //by name
-        WebElement inputElement = wd.findElement(By.name("LastName"));
-        WebElement inputElements1 = wd.findElement(By.cssSelector("[name='Name']"));
-        WebElement inputElement2 = wd.findElement(By.name("Phone"));
-        WebElement inputElement3 = wd.findElement(By.name("email"));
-        WebElement inputElement4 = wd.findElement(By.name("Address"));
-        WebElement inputElement5 = wd.findElement(By.name("description"));
 
         //By.linkText
         WebElement linkElement = wd.findElement(By.linkText("HOME"));
         WebElement linkElement2 = wd.findElement(By.linkText("ABOUT"));
-        WebElement linkElement3 = wd.findElement(By.linkText("CONTACTS"));
-        WebElement linkElement4 = wd.findElement(By.linkText("ADD"));
-
-
-
+      
     }
 }
