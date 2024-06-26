6e6ffc7c26bfb088ede358991558d7835d17c26c
Lidiia90 <liduska200412@gmail.com>
2024-06-26 14:34:30 +0300

    add xPath

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -20,8 +20,21 @@ public class HomeWork {
     public void homework1() {
 
         //by Tag name
-        WebElement button = wd.findElement(By.tagName("button"));
-        WebElement button1 = wd.findElement(By.cssSelector("button"));
+        WebElement element = wd.findElement(By.tagName("button"));
+        WebElement element1 = wd.findElement(By.cssSelector("button"));
+        WebElement element2 = wd.findElement(By.xpath("//button"));
+
+        WebElement element3 = wd.findElement(By.tagName("body"));
+        WebElement element4 = wd.findElement(By.cssSelector("body"));
+
+        WebElement element5 = wd.findElement(By.tagName("div"));
+        WebElement element6 = wd.findElement(By.cssSelector("div"));
+
+        WebElement element7 = wd.findElement(By.tagName("h1"));
+        WebElement element8 = wd.findElement(By.cssSelector("h1"));
+
+        WebElement element9 = wd.findElement(By.tagName("a"));
+        WebElement element10 = wd.findElement(By.cssSelector("a"));
 
         List<WebElement> a = wd.findElements(By.tagName("a"));
         List<WebElement> list = wd.findElements(By.cssSelector("a"));
@@ -32,13 +45,43 @@ public class HomeWork {
         //by Class
         List<WebElement> containerList = wd.findElements(By.className("container"));
         List<WebElement> containerList1 = wd.findElements(By.cssSelector(".container"));
+        WebElement element11 = wd.findElement(By.className("container"));
+        WebElement element12 = wd.findElement(By.cssSelector(".container"));
+        WebElement element13 = wd.findElement(By.xpath("//*[@class='container']"));
+
+        WebElement element14 = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement element15 = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
+
+        WebElement element16 = wd.findElement(By.className("active"));
+        WebElement element17 = wd.findElement(By.cssSelector(".active"));
 
         //by id
-        WebElement container1 = wd.findElement(By.cssSelector("#root.container"));
+        WebElement element18 = wd.findElement(By.id("root"));
+        WebElement element19 = wd.findElement(By.cssSelector("#root.container"));
+        WebElement el31 = wd.findElement(By.xpath("//*[@id='root']"));
 
         //by attribute
         WebElement inputName = wd.findElement(By.cssSelector("[placeholder='Email']"));
         WebElement inputName1 = wd.findElement(By.cssSelector("[placeholder='Password']"));
+
+        WebElement element20 = wd.findElement(By.cssSelector("[href='/home']"));
+
+        WebElement element21 = wd.findElement(By.cssSelector("[name='email']"));
+        WebElement element22 = wd.findElement(By.name("email"));
+
+        WebElement element23 = wd.findElement(By.cssSelector("[placeholder='Email']"));
+        WebElement element24 = wd.findElement(By.xpath("//*[@placeholder='Email']"));
+
+        WebElement element25 = wd.findElement(By.cssSelector("[placeholder^='Em'"));
+        WebElement element26 = wd.findElement(By.xpath("//*[starts-with(@placeholder,'Em')]"));
+
+        WebElement element27 = wd.findElement(By.cssSelector("[placeholder$='il'"));
+        WebElement element28 = wd.findElement(By.xpath("//*[contains(@placeholder,'il')]"));
+
+        WebElement element29 = wd.findElement(By.cssSelector("[placeholder*='ma'"));
+        WebElement element30 = wd.findElement(By.xpath("//*[contains(@placeholder,'ma')]"));
+
+        //password
 
         //By.linkText
         WebElement linkElement = wd.findElement(By.linkText("HOME"));
--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,7 +16,12 @@ public class Index {
         wd = new ChromeDriver();
         wd.get("file:///Users/lidashpektorovska/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8C%20%D1%81%20%D0%A0%D0%B0%D0%B1%20%D1%81%D1%82%D0%BE%D0%BB%D0%B0/HI%20TECH/AVTOMATION%20%D0%BF%D0%BE%D0%B2%D1%82%D0%BE%D1%80%D0%BD%D1%8B%D0%B8%CC%86%20%D0%BA%D1%83%D1%80%D1%81/LESSON/2%20lesson/21.index.html");
     }
-
+    @Test
+    public void tableTest(){
+        //Canada
+        WebElement canada = wd.findElement(By.cssSelector("tr:nth-child(3) td:last-child"));
+        Assert.assertEquals(canada.getText(), "Canada");
+    }
 
     @Test
     public void cssLocators() {
@@ -30,7 +36,6 @@ public class Index {
         WebElement input = wd.findElement(By.tagName("input"));
         WebElement input1 = wd.findElement(By.cssSelector("input"));
 
-
         //by Class
         WebElement divContainer = wd.findElement(By.className("container"));
         WebElement divContainer1 = wd.findElement(By.cssSelector(".container"));
@@ -38,25 +43,29 @@ public class Index {
         List<WebElement> navList = wd.findElements(By.className("nav-item"));
         List<WebElement> navList1 = wd.findElements(By.cssSelector(".nav-item"));
 
-
         //by Id
         WebElement nav = wd.findElement(By.id("nav"));
         WebElement nav1 = wd.findElement(By.cssSelector("#nav"));
 
-
         //by attribute
         WebElement inputName = wd.findElement(By.cssSelector("[placeholder='Type your name']"));
-        WebElement a2 = wd.findElement(By.cssSelector("[href='#item3']"));
+       //start
+        WebElement inputName1 = wd.findElement(By.cssSelector("[placeholder^='Type']"));
+        //end
+        WebElement inputName2 = wd.findElement(By.cssSelector("[placeholder$='name']"));
+        //contains
+        WebElement inputName3 = wd.findElement(By.cssSelector("[placeholder*='your']"));
 
+        WebElement a2 = wd.findElement(By.cssSelector("[href='#item3']"));
 
         //by name
         WebElement inputS = wd.findElement(By.name("surename"));
         WebElement inputS1 = wd.findElement(By.cssSelector("[name='surename']"));
 
-
         //By.linkText && By.partialLinkText
         WebElement a3  = wd.findElement(By.linkText("Item 1"));
         WebElement a4  = wd.findElement(By.partialLinkText("m 1"));
+
 
     }
 }
